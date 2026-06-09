1eda80df3485b1725edbd9bfc9be8d126d8f9bd4
NadiaSuganiaka <suhaniaka.nadia@gmail.com>
2026-06-09 10:10:37 +0300

    feat: update requirements

--- config/settings.py
+++ config/settings.py
@@ -32,19 +32,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-   'channels',
-   'django.contrib.admin',
-   'django.contrib.auth',
-   'django.contrib.contenttypes',
-   'django.contrib.sessions',
-   'django.contrib.messages',
-   'django.contrib.staticfiles',
-   'django_cleanup.apps.CleanupConfig',
-   'widget_tweaks',
-   'main',
-   'accounts',
-   'photos',
-   'posts'
+    'channels',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_cleanup.apps.CleanupConfig',
+    'widget_tweaks',
+    'main',
+    'accounts',
+    'photos',
+    'posts'
 ]
 
 
